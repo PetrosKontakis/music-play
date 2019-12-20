8593a51a50dbd5e83a4e981d57bd12832306ef70
petros_ <p.kontakis@athlenda.com>
2019-12-20 02:53:28 +0200

    Updates

---
+++ precache-manifest.0c02778e78ee42ad9f57c42a4669b1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6edfe0bc97f91e4e9f8240233b10c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71e75fe923b09190cf2",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "97a84da3531bc5edd1e2",
+    "url": "/static/css/main.2d74a05a.chunk.css"
+  },
+  {
+    "revision": "b71e75fe923b09190cf2",
+    "url": "/static/js/2.c6a22f33.chunk.js"
+  },
+  {
+    "revision": "d1f306884815a0ffc76c404eb40926a6",
+    "url": "/static/js/2.c6a22f33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97a84da3531bc5edd1e2",
+    "url": "/static/js/main.fb3aadcb.chunk.js"
+  },
+  {
+    "revision": "2ec13fbd08f4e7778fe2",
+    "url": "/static/js/runtime-main.fef7215a.js"
+  }
+]);
