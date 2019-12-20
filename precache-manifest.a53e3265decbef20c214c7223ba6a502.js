a271755f4312d93c527a4c6a856eb0867bdc74c3
petros_ <p.kontakis@athlenda.com>
2019-12-20 03:04:37 +0200

    Updates

---
+++ precache-manifest.a53e3265decbef20c214c7223ba6a502.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae8a6826b24b42bbacb1b834fecbf31",
+    "url": "/music-play/index.html"
+  },
+  {
+    "revision": "ff7d71a1aefa40720377",
+    "url": "/music-play/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "76276d9f6a06f439f400",
+    "url": "/music-play/static/css/main.2d74a05a.chunk.css"
+  },
+  {
+    "revision": "ff7d71a1aefa40720377",
+    "url": "/music-play/static/js/2.bc88d812.chunk.js"
+  },
+  {
+    "revision": "d1f306884815a0ffc76c404eb40926a6",
+    "url": "/music-play/static/js/2.bc88d812.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76276d9f6a06f439f400",
+    "url": "/music-play/static/js/main.64d6bcd4.chunk.js"
+  },
+  {
+    "revision": "f2b9e09cebbd90b32bcb",
+    "url": "/music-play/static/js/runtime-main.57015354.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c02778e78ee42ad9f57c42a4669b1de.js"
+  "/music-play/precache-manifest.a53e3265decbef20c214c7223ba6a502.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music-play/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
